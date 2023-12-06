dff7ce8e4998b241ed9ff501a6cf6b764ba0d34e
matthewstevens <mstevens@ithaca.edu>
2023-12-05 21:01:29 -0500

    Added stuff for profile page

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
+from hashlib import md5
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 
 """class Post(models.Model):
@@ -35,12 +37,18 @@ class User(UserMixin, db.Model):
     lastname = db.Column(db.String(64), index=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(128), index=True, unique=True)
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 @login.user_loader
 def load_user(id):
