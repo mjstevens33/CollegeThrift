601a0f986ca98c95514c03734ecab3dce48155a2
lerlichsondeliz <lerlichsondeliz@ithaca.edu>
2023-12-05 22:37:22 -0500

    another commite before I might break things

--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,24 @@ from app.models import User, Post, Category
 from flask_login import login_user, current_user, logout_user, login_required
 
 
-
 @app.route('/')
 @app.route('/Home')
 def Home():
     posts = Post.query.order_by(Post.created_at.desc()).limit(6).all()
     return render_template('index.html', title='Home', posts=posts)
 
+
 @app.route('/post/<int:post_id>')
 def post_detail(post_id):
     post = Post.query.get_or_404(post_id)  # Fetch the post or return 404 if not found
     return render_template('post_detail.html', post=post)  # Assume you have a template named 'post_detail.html'
+
+
+@app.route('/browse')
+def browse():
+    items = Post.query.all()  # Fetch all items from the database
+    return render_template('browse.html', items=items)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -36,11 +43,13 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -55,4 +64,3 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
-
