bf6faab04ee0a62be96a42e990750e9959e8619d
matthewstevens <mstevens@ithaca.edu>
2023-12-10 19:58:54 -0500

    Did alot to make it function

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'collegeThrift.db')
 
+    from dotenv import load_dotenv
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    load_dotenv(os.path.join(basedir, '.env'))
+
+
+
